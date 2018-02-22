dd53ab64857458e052b7dd96d2b2a4df72addcea
Simon Sun <9262981@qq.com>
2018-02-22 22:20:35 +0800

    newAdd

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <router-link to="/test">/test</router-link>
+    <ul>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+var qs = require('qs');
+export default {
+  name: 'HelloWorld',
+  beforeCreate: function () {
+
+    let that = this
+    axios.post('https://y.freekeer.com/marketing/insensitive/npo/getAreas')
+      .then(function (response) {
+        console.log(response)
+        that.msg=response
+      })
+      .catch(function (response) {
+        location.href='/m/'
+        console.log(response);
+      })
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    Test
+    <router-link to="/">回到首页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      msg: 'Welcome to Your Test App'
+    }
+  }
+}
+</script>
